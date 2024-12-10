d7b1a56298739fd4e2c6705ea4edb01e02476a9b
Jannat <shahrinasabrin@gmail.com>
2024-12-10 19:02:43 +0600

    added: dashboards

--- app/app/student/page.tsx
+++ app/app/student/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+import { useValidate } from "@/lib/actions/auth/validate.get";
+
 export default function DashboardPage() {
-	return (
-		<div>
-			<h1>Student Dashboard</h1>
-		</div>
-	);
+  const { data } = useValidate();
+
+  const fullname =
+    data?.data?.profile?.first_name + " " + data?.data?.profile?.last_name;
+
+  return (
+    <>
+      <div className="flex flex-col items-center p-8">
+        <p className="text-3xl font-bold text-blue-500">{fullname}</p>
+        <p className="text-lg font-sans">Welcome to Student Dashboard</p>
+      </div>
+    </>
+  );
 }
--- lib/actions/auth/validate.get.ts
+++ lib/actions/auth/validate.get.ts
@@ -2,10 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import instance from "..";
 
 export const useValidate = () => {
-	return useQuery({
-		queryKey: ["validate"],
-		queryFn: () => {
-			return instance.get("/auth/validate");
-		},
-	});
+  return useQuery({
+    queryKey: ["validate"],
+    queryFn: () => {
+      return instance.get("/auth/validate");
+    },
+  });
 };
